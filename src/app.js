22fa89d4124e5c8cd6f57b358e68945c0d2b5e0c
HSimonVS <hebert.verdier@estudiantes.utec.edu.uy>
2024-07-01 18:50:16 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const excuse = document.getElementById("excuse");
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  excuse.innerHTML = generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return `${who[randomIndex(who)]} ${action[randomIndex(action)]} ${
+    what[randomIndex(what)]
+  } ${when[randomIndex(when)]}`;
+}
+
+function randomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
